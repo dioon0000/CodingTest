afddeba12955190c5f299cba2d5bcf4e88c6f1a8
쀼삡 <91640754+dioon0000@users.noreply.github.com>
2021-10-19 14:30:38 +0000

    21/10/19 update

---
+++ Silver/15651★ : N과 M(3).cpp
@@ -0,0 +1,25 @@
+//BackTracking PS (중복 순열)
+#include <iostream>
+#define MAX 7
+using namespace std;
+
+int n,m;
+int arr[MAX] = {0,};
+
+void dfs(int cnt){
+    if(cnt == m){
+        for(int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= n; i++){
+            arr[cnt] = i;
+            dfs(cnt+1);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    dfs(0);
+}
