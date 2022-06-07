7576d6a5781975802750c365ffbd95e6e5018972
쀼삡 <91640754+dioon0000@users.noreply.github.com>
2022-06-07 10:12:37 +0000

    백트래킹 PS

---
+++ Silver/#15655 : N과M(6).cpp
@@ -0,0 +1,37 @@
+//https://www.acmicpc.net/problem/15655
+#include <iostream>
+#include <algorithm>
+#define MAX 9
+using namespace std;
+
+int n, m;
+int arr[MAX] ={0, }, list[MAX] ={0, };
+bool visited[MAX] ={0, };
+void dfs(int num, int cnt){
+    //Base Condition
+    if(cnt == m){
+        for(int i=0; i<m; ++i)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    //Recursion Func
+    for(int i=num; i<n; ++i){
+        if(!visited[i]){
+            visited[i] = true;
+            arr[cnt] = list[i];
+            dfs(i+1, cnt+1);
+            visited[i] = false;
+        }
+    }
+}
+int main() {
+    cin >> n >> m;
+    for(int i=0; i<n; ++i)
+        cin >> list[i];
+
+    sort(list, list+n);
+    dfs(0, 0);
+    return 0;
+}
