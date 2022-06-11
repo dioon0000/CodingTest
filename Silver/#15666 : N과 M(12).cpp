d44bbbe8956af2cceeedab33b6ed3c0ff9cd7aba
쀼삡 <91640754+dioon0000@users.noreply.github.com>
2022-06-11 03:53:24 +0000

    백트래킹 PS

---
+++ Silver/#15666 : N과 M(12).cpp
@@ -0,0 +1,36 @@
+//https://www.acmicpc.net/problem/15666
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, arr[8], ans[8];
+
+void func(int num, int cnt){
+	if(cnt == m){
+		for(int i=0; i<m; ++i)
+			cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int preNum = -1;
+	for(int i=num; i<n; ++i){
+		if(arr[i]!=preNum){
+			ans[cnt] = arr[i];
+			preNum = ans[cnt];
+			func(i, cnt+1);
+		}
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for(int i=0; i<n; ++i)
+		cin >> arr[i];
+	sort(arr, arr+n);
+
+	func(0, 0);
+	return 0;
+}
