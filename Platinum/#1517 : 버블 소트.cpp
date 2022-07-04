f62d35e1b3325a5b2c08bf86241476f48d19055b
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-07-05 00:39:16 +0900

    MergeSort & Inversion Counting PS

---
+++ Platinum/#1517 : 버블 소트.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[500005], temp[500005], n;
+long long res=0;
+
+void Merge(int list[], int left, int mid, int right){
+	int i, j, k;
+	i = left;
+	j = mid+1;
+	k = left;
+
+	while(i<=mid && j<=right){
+		if(list[i] <= list[j])
+			temp[k++] = list[i++];
+		else{
+			temp[k++] = list[j++];
+			res+=(mid-i+1); //Inversion Counting (https://salepark.tistory.com/11)
+		}
+	}
+
+	while(i<=mid)
+		temp[k++] = list[i++];
+
+	while(j<=right)
+		temp[k++] = list[j++];
+
+	for(int x = left; x<=right; ++x)
+		list[x] = temp[x];
+}
+
+void MergeSort(int list[], int left, int right){
+	int mid;
+
+	if(left < right){
+		mid = (left+right)/2;
+
+		MergeSort(list, left, mid);
+		MergeSort(list, mid+1, right);
+		Merge(list, left, mid, right);
+	}
+
+	return;
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for(int i=0; i<n; ++i)
+		cin >> arr[i];
+
+	MergeSort(arr, 0, n-1);
+	cout << res;
+	return 0;
+}
