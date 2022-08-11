2ecedaa87c67c28c37aa3475ef19e80228125687
dioon0000 <dioon0000@naver.com>
2022-08-11 21:34:19 +0900

    DP LIS PS

---
+++ Silver/#11722_가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,31 @@
+﻿//https://www.acmicpc.net/problem/11722
+#include <bits/stdc++.h>
+#define MAX 1005
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int arr[MAX], dp[MAX];
+	int n, ans=0;
+
+	cin >> n;
+	for(int i=0; i<n; ++i)
+		cin >> arr[i];
+	dp[0] = 1;
+
+	for(int i=1; i<n; ++i){
+		dp[i] = 1;
+		for(int j=i-1; j>=0; --j){
+			if(arr[i] < arr[j]){
+				if(dp[j]+1 > dp[i])
+					dp[i] = dp[j]+1;
+			}
+		}
+	}
+
+	ans = *max_element(dp, dp+n);
+	cout << ans;
+	return 0;
+}
