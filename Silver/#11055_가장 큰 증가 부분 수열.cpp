6425d84e906b543c6dd8e317406cf57bb92f5bc4
dioon0000 <dioon0000@naver.com>
2022-08-11 21:32:21 +0900

    DP LIS PS

---
+++ Silver/#11055_가장 큰 증가 부분 수열.cpp
@@ -0,0 +1,32 @@
+﻿//https://www.acmicpc.net/problem/11055
+#include <iostream>
+#include <algorithm>
+#define MAX 1005
+using namespace std;
+
+int n, ans=0;
+int arr[MAX], dp[MAX];
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	//INPUT Info
+	cin >> n;
+	for(int i=0; i<n; ++i)
+		cin >> arr[i];
+	dp[0] = arr[0];
+
+	//O(n^2)
+	for(int i=1; i<n; ++i){
+		dp[i] = arr[i];
+		for(int j=i-1; j>=0; --j){
+			//현재 값이 이전 값보다 크면서 현재 dp[i]값이 dp[j]+arr[i]보다 작다면 값을 변경해준
+			if(arr[i] > arr[j] && dp[i] < dp[j] + arr[i])
+				dp[i] = dp[j] + arr[i];
+		}
+	}
+
+	ans = *max_element(dp, dp+n);
+	cout << ans;
+	return 0;
+}
