0b89194261a4a313603abb59d256ad58edc8e80f
dioon0000 <dioon0000@naver.com>
2022-10-05 20:53:22 +0900

    Math PS

---
+++ Bronze/#17362_수학은 체육과목입니다2.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, ans;
+	cin >> n;
+	ans=n%8;
+	
+	if(ans == 1)
+		cout << '1';
+	else if(ans == 0 || ans == 2)
+		cout << '2';
+	else if(ans == 3 || ans == 7)
+		cout << '3';
+	else if(ans == 4 || ans == 6)
+		cout << '4';
+	else
+		cout << '5';
+	return 0;
+}
