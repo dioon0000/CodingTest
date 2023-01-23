a7e907d506065d90fc5233b09ee8af36f77e4d4a
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2023-01-23 23:20:06 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1000001];
+
+int main() {
+    
+    cin >> n;
+    arr[1] = 0;
+    arr[2] = 1;
+    arr[3] = 1;
+    for (int i = 4; i <= n; i++){
+        arr[i] = arr[i-1] + 1;
+        if (i % 3 == 0) {
+            arr[i] = min(arr[i], arr[i/3] + 1);
+        } if (i % 2 == 0) {
+            arr[i] = min(arr[i], arr[i/2] + 1);
+        }
+    }
+    cout << arr[n] << "\n";
+    return 0;
+}
