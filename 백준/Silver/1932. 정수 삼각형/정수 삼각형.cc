fa610004d560e9da8b84916239eb3ad03f74d771
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2023-09-07 18:54:14 +0900

    [Silver I] Title: 정수 삼각형, Time: 12 ms, Memory: 2812 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,42 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[500][500];
-int n, maxVal=0;
-
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	
+	int n;
 	cin >> n;
-	for(int i=0; i<n; ++i){
+	vector<vector<int>> dp(n); //2차원 vector
+	
+	for(int i=0; i<n; ++i)
 		for(int j=0; j<=i; ++j){
-			cin >> arr[i][j];
+			int x;
+			cin >> x;
+			dp[i].push_back(x);
 		}
-	}
 
-	for(int i=1; i<n; ++i){
-		for(int j=0; j<=i; ++j){
-			if(j==0)
-				arr[i][j]+=arr[i-1][j];
-			else if(j==i)
-				arr[i][j]+=arr[i-1][j-1];
-			else{
-				int left, right;
-				left = arr[i][j]+arr[i-1][j];
-				right = arr[i][j]+arr[i-1][j-1];
-				if(left<=right)
-					arr[i][j] = right;
-				else
-					arr[i][j] = left;
-			}
-		}
-	}
+    //DP 알고리즘의 최적의 원칙을 활용한 풀이방법
+	for(int i=n-2; i>=0; --i)
+		for(int j=0; j<=i; ++j)
+			dp[i][j] = max(dp[i+1][j], dp[i+1][j+1])+dp[i][j];
 
-	for(int i=0; i<n; ++i){
-		if(maxVal < arr[n-1][i])
-			maxVal = arr[n-1][i];
-	}
-	cout << maxVal;
+	cout << dp[0][0];
 	return 0;
 }
