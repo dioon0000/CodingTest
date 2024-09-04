d378fac0f69784095eaefb0276876b7735fcd7bd
dioon0000 <dioon0000@naver.com>
2024-09-05 00:03:32 +0900

    Create a Baekjoon Solve folder that you solved in the past

---
+++ 초급풀이/Bronze/#5523_입실 관리.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<string>v(n);
+
+	for(int i=0; i<n; ++i){
+		cin >> v[i];
+		transform(v[i].begin(), v[i].end(), v[i].begin(), ::tolower);
+	}
+
+	for(int i=0; i<n; ++i)
+		cout << v[i] << '\n';
+
+	return 0;
+}
---
+++ 초급풀이/Silver/#11403_경로 찾기.cpp
@@ -0,0 +1,37 @@
+﻿//https://www.acmicpc.net/problem/11403
+#include <bits/stdc++.h>
+#define MAX 105
+using namespace std;
+
+int graph[MAX][MAX];
+int n;
+
+void floyd(){
+	for(int k=0; k<n; ++k)
+		for(int i=0; i<n; ++i)
+			for(int j=0; j<n; ++j)
+				if(graph[i][k] && graph[k][j])
+					graph[i][j] = 1;
+
+	return;
+}
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	//graph 정보 입력
+	for(int i=0; i<n; ++i)
+		for(int j=0; j<n; ++j)
+			cin >> graph[i][j];
+	
+	floyd();
+
+	for(int i=0; i<n; ++i){
+		for(int j=0; j<n; ++j){
+			cout << graph[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
