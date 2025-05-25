a518ae010da51366c8a115e7dceebf005b55e76b
DiooN <91640754+dioon0000@users.noreply.github.com>
2025-05-25 19:24:59 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n1, int n2){
+	int ans;
+	return n1 + n2;
+}
+int main() {
+	ios::sync_with_stdio(0);
+
+	int n1, n2;
+	cin >> n1 >> n2;
+	cout << solution(n1, n2);
+	return 0;
+	
+}
