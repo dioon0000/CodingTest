7e1e27ea035324da853c5786419879995c7727d4
DiooN <91640754+dioon0000@users.noreply.github.com>
2025-05-30 20:37:35 +0900

    간결한 풀이 추가

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -21,3 +21,29 @@ int solution(int n, int m, vector<int> section) {
     }
     return answer;
 }
+
+/*
+//더 나은 풀이
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 1;
+
+    int pivot = section[0];
+    //pivot+m은 pivot부터 m까지 색칠하는 부분보다 한칸 앞을 위치한다
+    for(const auto v : section){
+        if(v < pivot + m){
+            continue;
+        }
+        else{
+            pivot = v;
+            answer++;
+        }
+    }
+
+    return answer;
+}
+*/
