0075771bbbbe04b764247287a080da65c9246c33
DiooN <91640754+dioon0000@users.noreply.github.com>
2025-06-20 09:27:22 +0900

    [level 2] Title: n^2 배열 자르기, Time: 28.90 ms, Memory: 24.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+	vector<int> answer;
+
+	for(long long i=left; i<=right; ++i)
+	{
+		long long x = i / n;
+		long long y = i % n;
+		if(x >= y) answer.push_back(x+1);
+		else answer.push_back(y+1);
+	}
+	return answer;
+}
+
+int main() {
+	solution(4, 4, 6);
+	return 0;
+}
