4f8a789daf5f80ec98e53fb275fdff3fd0a34b6f
DiooN <91640754+dioon0000@users.noreply.github.com>
2025-07-10 16:49:37 +0900

    [level 2] Title: 숫자 변환하기, Time: 7.22 ms, Memory: 5.16 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -1,17 +1,6 @@
-#include <iostream>
 #include <queue>
 using namespace std;
 
-/*
-자연수 x를 y로 변환하려고 합니다. 사용할 수 있는 연산은 다음과 같습니다.
-
-x에 n을 더합니다
-x에 2를 곱합니다.
-x에 3을 곱합니다.
-자연수 x, y, n이 매개변수로 주어질 때,
-x를 y로 변환하기 위해 필요한 최소 연산 횟수를 return하도록
-Solution 함수를 완성해주세요. 이때 x를 y로 만들 수 없다면 -1을 return 해주세요.
-*/
 vector<bool>visited(1000001, false);
 queue<pair<int, int>> q;
 int bfs(queue<pair<int,int>>& q, int y, int n) {
@@ -47,11 +36,5 @@ int solution(int x, int y, int n) {
     q.push({ x, 0 });
     answer = bfs(q,y,n);
 
-    cout << answer;
     return answer;
-}
-
-int main() {
-    solution(10, 40, 5);
-    return 0;
 }
