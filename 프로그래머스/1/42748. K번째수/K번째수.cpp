919b314c92dccda2614e6cabbf4fe066bebd54c0
DiooN <91640754+dioon0000@users.noreply.github.com>
2025-07-10 10:30:30 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,26 +1,26 @@
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    //command[][0] = i, command[][1] = j, command[][2] = k
-    //command에 따라 잘려질 문자열 보관 command가 끝나면 초기화 해줘야함
-    vector<int> tmp;
-    //tmp 과정이 끝나면 저장할 벡터
-    vector<int> answer;
+	vector<int> answer;
+	vector<int> temp;
     
-    for(int i=0; i<commands.size(); ++i){
-        int k = 0;
-        for(int j=commands[i][0]-1; j<commands[i][1]; ++j){
-            //cout << j << ' ';
-            k = commands[i][2];
-            tmp.push_back(array[j]);
-        }
-        sort(tmp.begin(), tmp.end());
-        answer.push_back(tmp[k-1]);
-        tmp.clear();
-    }
-    return answer;
+	for(int i=0; i<commands.size(); ++i) {
+		for(int j=commands[i][0]-1; j<commands[i][1]; ++j)
+			temp.push_back(array[j]);
+		int idx = commands[i][2];
+        
+		sort(temp.begin(), temp.end());
+		answer.push_back(temp[idx-1]);
+		temp.clear();
+	}
+	return answer;
+}
+
+int main() {
+	solution({ 1, 5, 2, 6, 3, 7, 4 }, { {2,5,3},{4,4,1},{1,7,3} });
+	return 0;
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.02 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 28일 11:11:32
+2025년 07월 10일 10:30:29
 
 ### 문제 설명
 
