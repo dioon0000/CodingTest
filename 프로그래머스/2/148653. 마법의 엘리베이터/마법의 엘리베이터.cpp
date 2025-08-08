be7ef3949337a90c58e8d3dc355d9dc8119c1205
DiooN <91640754+dioon0000@users.noreply.github.com>
2025-08-08 09:38:42 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int storey) {
+	int answer = 0;
+	while(storey>0) {
+		int val;
+		val = storey % 10;
+
+		if(val>5) { 
+			int addVal = 10-val;
+			answer += addVal;
+			storey+=addVal;
+		}
+		else if(val == 5) {
+			int nextVal = storey%100/10;
+			if(nextVal>=5) {
+				int addVal = 10-val;
+				answer += addVal;
+				storey+=addVal;
+			}
+			else {
+				answer += val;;
+			}
+		}
+		else { 
+			answer += val; 
+		}
+
+		storey/=10;
+	}
+
+	cout << answer;
+	return answer;
+}
+
+int main() {
+	solution(5555);
+	return 0;
+}
