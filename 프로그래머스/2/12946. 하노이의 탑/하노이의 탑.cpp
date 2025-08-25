77131858555664a8aa53816e45a59f36db4e8fae
DiooN <91640754+dioon0000@users.noreply.github.com>
2025-08-25 11:14:14 +0900

    [level 2] Title: 하노이의 탑, Time: 23.44 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -2,35 +2,19 @@
 
 using namespace std;
 
-void hanoi(int n, int from, int tmp, int to, vector<vector<int>>& answer) {
-    vector<int> v;
-
-    if(n==1) {
-        // from -> to
-        v.push_back(from);
-        v.push_back(to);
-        answer.push_back(v);
+void hanoi(int n, int go, int to, vector<vector<int>>& answer) {
+    if (n == 1) {// 쌓인 원판이 한개라면 
+        answer.push_back({go, to});
+        return ;
     }
-    else {
-        // N-1개를 from -> tmp
-        hanoi(n-1, from, to, tmp, answer);
-
-        //from에 있는 1개를
-        //from -> to
-        v.push_back(from);
-        v.push_back(to);
-        answer.push_back(v);
-
-        // tmp의 N-1개 tmp -> to
-        hanoi(n-1, tmp, from, to, answer);
-    }
-
-    return;
+    hanoi(n-1, go, 6-go-to, answer);// n-1 개의 원판을 시작점, 목표점을 제외한 곳으로 이동
+    hanoi(1, go, to, answer);// n번째 원판을 목표지점으로 이동
+    hanoi(n-1, 6-go-to, to, answer);// n-1 개의 원판을 목표점으로 이동
 }
 
 vector<vector<int>> solution(int n) {
     vector<vector<int>> answer;
-    hanoi(n, 1, 2, 3, answer);
+    hanoi(n, 1, 3, answer);
 
     return answer;
 }
